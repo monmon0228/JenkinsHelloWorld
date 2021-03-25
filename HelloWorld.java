fd5c93515e343b25cef3d0612d869cb21d5b3ece
Colitoy, Monica <monica.colitoy@accenture.com>
2021-03-25 17:20:04 +0800

    Adding new Files

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("******** Its a Beautiful Day ! *******************");
+		System.out.println("Helloooooooooooooooooooooooooo.............");
+		System.out.println("How are you????????????");
+	}
+
+}
